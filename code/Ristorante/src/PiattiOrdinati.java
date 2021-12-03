0eb433553157a799bc16b8dca854bec7cef963b4
Mattia-Giudici <m.giudici9@studenti.unibg.it>
2021-12-03 11:24:41 +0100

    Classe piatto e Piatto ordinato

--- code/Ristorante/src/PiattiOrdinati.java
+++ code/Ristorante/src/PiattiOrdinati.java
@@ -1,4 +1,53 @@
 
 public class PiattiOrdinati {
 
+	protected Piatto piatti[];
+	protected int Tavolo; 
+	protected int num_Persone;
+	
+	public PiattiOrdinati(Piatto[] piatti, int tavolo, int num_Persone) {
+	
+		this.piatti = piatti;
+		Tavolo = tavolo;
+		this.num_Persone = num_Persone;
+	}
+
+	public PiattiOrdinati(int tavolo, int num_Persone) {
+		
+		Tavolo = tavolo;
+		this.num_Persone = num_Persone;
+	}
+	
+	public PiattiOrdinati(Piatto piatti, int tavolo, int num_Persone) {
+		
+		this.piatti[0] = piatti;
+		Tavolo = tavolo;
+		this.num_Persone = num_Persone;
+	}
+	
+		public void AddPiatto(Piatto piatto) {
+			piatti[piatti.length+1]=piatto;
+			System.out.println("piatto aggiunto correttamente");
+		}
+		public void RemovePiatto(Piatto piatto) {
+			boolean check = false;
+			for(int i= 0; i<=piatti.length; i++) {
+				if(check=false) {
+				if(piatti[i].Nome==piatto.Nome) {
+					Piatto momento[]=piatti;
+					for(int k=0; k<i;k++) {
+						piatti[k]= momento[k];
+						
+					}
+					for(int k=i+1; k<momento.length; k++) {
+						piatti[k-1]=momento[k];
+					}
+					check=true;
+					System.out.println("piatto rimosso");
+					}
+				
+					}
+			}
+		}
+	
 }
--- code/Ristorante/src/Piatto.java
+++ code/Ristorante/src/Piatto.java
@@ -1,4 +1,48 @@
 
 public class Piatto {
-
+	protected String Nome; 
+	protected Ingrediente ingredienti[];
+	float Prezzo;
+	public Piatto(String nome, Ingrediente[] ingredienti, float prezzo) {
+		Nome = nome;
+		this.ingredienti = ingredienti;
+		Prezzo = prezzo;
+	} 
+		
+	public Piatto(String nome, Ingrediente ingredienti, float prezzo) {
+		Nome = nome;
+		this.ingredienti[0]= ingredienti;
+		Prezzo = prezzo;
+	} 
+	
+	public void ModificaIngrediente(Ingrediente ingrediente, boolean AddRemove){ //true = aggiungi ingrediente, false = rimuovi
+		if (AddRemove == true) {
+			this.ingredienti[ingredienti.length+1] = ingrediente;
+			System.out.println("Ingrediente aggiunto correttamente");
+		}else {
+			boolean controllo = false;
+			for(int i = 0; i<= this.ingredienti.length; i++) {
+				if(ingredienti[i].Nome ==ingrediente.Nome) {
+					controllo = true;
+					Ingrediente momentaneo[]= ingredienti;
+					for(int k = 0; k<i;k++) {
+						ingredienti[k]= momentaneo[k];
+					}
+					for (int k =i+1;k<momentaneo.length; k++) {
+						ingredienti[k-1]=momentaneo[k];
+					}
+				}
+			}
+			if(controllo == true)
+				System.out.println("ingrediente Rimosso con successo");
+			else
+				System.out.println("ingrediente non trovato");
+		}
+		
+		
+	}
+	public void ModificaPrezzo(float NuovoPrezzo) {
+		this.Prezzo = NuovoPrezzo; 
+		System.out.println("il prezzo è stato correttamente aggiornato");
+	}
 }
