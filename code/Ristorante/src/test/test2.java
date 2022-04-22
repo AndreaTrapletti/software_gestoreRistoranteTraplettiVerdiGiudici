90dac7a62e151476cabb0f7a906af9c65df48461
MicheleVerdi <92978375+MicheleVerdi@users.noreply.github.com>
2022-04-22 17:16:36 +0200

    commit test

--- code/Ristorante/src/test/test2.java
+++ code/Ristorante/src/test/test2.java
@@ -1,22 +1,23 @@
 package test;
 import ristorante.*;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-
 import prog.utili.Data;
+class test2 {
 
-class test1 {
 	Ingrediente i = new Ingrediente("mais", 2, new Data(22, 06, 2000), 5);
+	Piatto popcorn = new Piatto("popcorn", i, 3);
 	@Test
 	void test() {
-
-		assertEquals(5, i.getPrezzo());
+		popcorn.ModificaPrezzo(3.25)
+		assertEquals(3.25, popcorn.prezzo);
 	}
 	void test2() {
-
-		assertEquals(2, i.getQtd());
+		Ingrediente i2 = new Ingrediente("burro", 2, new Data(22, 06, 2000), 5);
+		popcorn.ModificaIngrediente( i2, true)
+		
+		assertEquals("burro", i2.ingredienti.elementAt(1).name());
 	}
 	void test3() {
 
@@ -32,4 +33,5 @@ class test1 {
 
 		assertEquals(true, i.ControlloQuantita());
 	}
+
 }
